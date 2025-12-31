a1eaee112fa41ac431b66f7be24eafbd9f8b6571
lingar <yimprogramming@gmail.com>
2025-12-31 12:38:22 +0200

    Begin to create the handle change

--- src/app/pages/components/ApartmentClient.jsx
+++ src/app/pages/components/ApartmentClient.jsx
@@ -8,10 +8,11 @@
 //         isCommercial: false,
 //         price: 2600
 //     }
-
+'use client'
 
 import {generateId} from "@/app/global-services/utils";
-import {Box} from "@mui/material";
+import {Box, TextField} from "@mui/material";
+import {useEffect, useState} from "react";
 
 const mockData = [
 
@@ -55,8 +56,46 @@ const mockData = [
 ]
 const ApartmentClient = () => {
 
+    //PROPERTIES
+    const [apartments, setApartments] = useState([]);//empty on default - for preventing crushes
 
-    return(
+    const [newApartment, setNewApartment] = useState({
+        id: null,
+        address: "",
+        rooms: 0,
+        floor: 0,
+        type: "",
+        isCommercial: false,
+        price: 0
+    });
+    //FUNCTIONS
+
+    const handleNewApartmentChanges = (ev) => {
+        const {name, value} = ev.target;
+        if(name !== "isCommercial"){
+            setNewApartment(previousState=>{
+                return {
+                    ...previousState,//we copying all the object
+                    [name]: value//changing only the specific property
+                }
+            })
+        }else{//handling checkbox
+            setNewApartment(previousState=>{
+                return {
+                    ...previousState,//we copying all the object
+                    [name]: ev.target.checked//changing only the specific property
+                }
+            })
+        }
+    }
+
+    //EFFECTS
+    useEffect(() => {
+
+        setApartments(mockData);
+
+    }, []);
+    return (
         <>
             <Box component={"div"}>
 
@@ -67,6 +106,19 @@ const ApartmentClient = () => {
 
                 <h3>All Apartments: </h3>
 
+
+                {mockData.map(item => {
+
+                    return <p key={item.id}>
+                        ID = {item.id} | {item.address}, {item.rooms} rooms, floor {item.floor},
+                        type is {item.type}, <b>{item.isCommercial ? "Allowed for Commercial use" :
+                        "Only for private use"}</b>,
+                        &nbsp;
+                        {item.price}$
+
+                    </p>
+
+                })}
                 {/*{
         id: generateId(),
         address: "HA'Ary 5",
@@ -76,18 +128,24 @@ const ApartmentClient = () => {
         isCommercial: false,
         price: 2600
     },*/}
-                {mockData.map(item=>{
+                <h2>Add Apartments</h2>
+                <Box>
 
-                    return <p key = {item.id}>
-                        ID = {item.id} | {item.address}, {item.rooms} rooms, floor {item.floor},
-                        type is {item.type}, <b>{item.isCommercial? "Allowed for Commercial use" :
-                        "Only for private use"}</b>,
-                        &nbsp;
-                        {item.price}$
+                    {/*The input of mui*/}
+                    <TextField
+                        variant={"outlined"}
+                        label={"Address"}
+                        value={newApartment.address}
+                        name="address"
+                        onChange={handleNewApartmentChanges}
 
-                    </p>
+                    >
 
-                })}
+
+                    </TextField>
+
+
+                </Box>
 
             </Box>
 
