1c88da98547f61ee41b0d7a061a1a58a99de8dd5
lingar <yimprogramming@gmail.com>
2026-01-07 13:51:44 +0200

    Preparing client structure

---
+++ src/app/pages/docs/crud-examples/server/page.jsx
@@ -0,0 +1,28 @@
+import {Box} from "@mui/material";
+import ApartmentClient from "@/app/pages/components/ApartmentClient";
+
+
+export const Inner=()=> {
+
+    return (
+        <Box component={"div"} p={"40px"}>
+
+            <h2>Connected To Server CRUD</h2>
+            <p>
+
+                This data is connected to server crud <br/>
+                http://localhost:3000/api/apartment
+            </p>
+            <ApartmentClient/>
+            <ol>
+                {/*<li><Link href={`${pathname}/inner`}>Demonstrate Simple inner CRUD with client fake data.</Link></li>*/}
+                {/*<li><Link href={`${pathname}/server`}>Demonstrate  CRUD to the server - but without DB</Link></li>*/}
+                {/*<li><Link href={`${pathname}/db`}>Demonstrate  CRUD to the server - With full DB support</Link></li>*/}
+
+            </ol>
+
+
+        </Box>
+    );
+}
+export default Inner;
