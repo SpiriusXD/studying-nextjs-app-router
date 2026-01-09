5589fc65b09e0515cdeed54aff56cc1fad5793a3
lingar <yimprogramming@gmail.com>
2026-01-09 13:27:45 +0200

    card poc

--- src/app/pages/draft/page.js
+++ src/app/pages/draft/page.js
@@ -1,14 +1,61 @@
-import { Box, Button } from "@mui/material";
+import Image from "next/image";
+import {Box} from "@mui/material";
+
+export default function Draft() {
+
+    const length = 9;
+    const cardLength = 200;
 
 
-export default function Page1() {
-  return (
-    <Box >
+    return (
+        <>
+            <h1>Here will come the Draft work</h1>
 
-      <h1>Here will come the Draft work</h1>
-    <Button variant="contained">ABC</Button>
+            <h2>Let's start make POC for memoroy-game</h2>
 
-   
-    </Box>
-  );
-}
+            <p>First, let's show Grid of cards: </p>
+
+            <Box component={"div"}
+                 sx={{
+                     display: "grid",
+                     gridTemplateColumns: "repeat(3, 1fr)",
+                     gridTemplateRows: "repeat(3, 1fr)",
+                     gap: "50px",
+                     padding: 4,
+                     width: "fit-content",
+                     border: "2px solid red"
+
+
+                 }}>
+
+                {Array.from({length: length}).map((_, i) => (
+
+                    <Box component={"div"} sx={{
+                        width: cardLength, height: cardLength,
+                        border: "1px solid black",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        fontFamily: "san-serif",
+
+
+                        fontSize: "6rem"
+
+                    }}
+                    key = {i}>
+                    <strong>
+                        {i +1 }
+                    </strong>
+
+                    </Box>
+                ))}
+            </Box>
+
+
+            </>
+
+
+            );
+
+
+            }
